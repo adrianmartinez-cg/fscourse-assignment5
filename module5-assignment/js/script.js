4220b8a242f6e73fd556880725e206cb10c79d20
Pedro Adrian <pedro.martinez@ccc.ufcg.edu.br>
2022-05-15 22:01:11 -0300

    Retrieving random single category when clicking specials tile

--- module5-assignment/js/script.js
+++ module5-assignment/js/script.js
@@ -40,9 +40,8 @@ var showLoading = function (selector) {
 // with propValue in given 'string'
 var insertProperty = function (string, propName, propValue) {
   var propToReplace = "{{" + propName + "}}";
-  string = string
-    .replace(new RegExp(propToReplace, "g"), propValue);
-  return string;
+  var stringRet = string.replace(new RegExp(propToReplace, "g"), propValue);
+  return stringRet;
 };
 
 // Remove the class 'active' from home and switch to Menu button
@@ -102,8 +101,7 @@ function buildAndShowHomeHTML (categories) {
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories)["short_name"];
-
-
+      
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
       // Look through this code for an example of how to do use the insertProperty function.
@@ -115,7 +113,8 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName",chosenCategoryShortName);
+      var homeHtmlToInsertIntoMainPage = homeHtml;
+      homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlToInsertIntoMainPage,"randomCategoryShortName","'"+ chosenCategoryShortName +"'");
 
 
       // TODO: STEP 4: Insert the produced HTML in STEP 3 into the main page
